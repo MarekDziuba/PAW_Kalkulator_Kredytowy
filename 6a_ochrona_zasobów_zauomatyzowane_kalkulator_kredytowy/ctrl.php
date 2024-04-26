39088b84c7aed2d8c0d2627cc32f40b6bb4bc4ad
MarekDziuba <146839632+MarekDziuba@users.noreply.github.com>
2024-04-27 00:44:36 +0200

    Add files via upload

---
+++ 6a_ochrona_zasobów_zauomatyzowane_kalkulator_kredytowy/ctrl.php
@@ -0,0 +1,20 @@
+<?php
+// Skrypt kontrolera głównego uruchamiający określoną
+// akcję użytkownika na podstawie przekazanego parametru
+
+//każdy punkt wejścia aplikacji (skrypt uruchamiany bezpośrednio przez klienta) musi dołączać konfigurację
+require_once 'init.php';
+
+getConf()->login_action = 'login';
+
+switch ($action) {
+	default :
+		control('app\\controllers', 'CalcCtrl',		'generateView', ['user','admin']);
+	case 'login': 
+		control('app\\controllers', 'LoginCtrl',	'doLogin');
+	case 'calcCompute' : 
+		//zamiast pierwszego parametru można podać null lub '' wtedy zostanie przyjęta domyślna przestrzeń nazw dla kontrolerów
+		control(null, 'CalcCtrl',	'process',		['user','admin']);
+	case 'logout' : 
+		control(null, 'LoginCtrl',	'doLogout',		['user','admin']);
+}
